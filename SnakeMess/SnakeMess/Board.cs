2ec2e8e9d08a1cb8a24fb8d472a1450726c3a1c4
Henrik <hsn95@live.com>
2018-10-05 13:45:49 +0200

    Made stuff
    Made a Point class, Apple class, Board class and an empty GameConsole class :+1:

---
+++ SnakeMess/SnakeMess/Board.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace SnakeMess
+{
+	class Board
+	{
+		private int _width, _height;
+		public int Width
+		{
+			get
+			{
+				return _width;
+			}
+			set
+			{
+				_width = value;
+			}
+		}
+		public Board(int width = 1, int height = 1)
+		{
+			_width = width;
+			_height = height;
+		}
+
+	}
+}
---
+++ SnakeMess/SnakeMess/Point.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace SnakeMess
+{
+	class Point
+	{
+		private int _x, _y;
+		public int X
+		{
+			get
+			{
+				return _x;
+			}
+			set
+			{
+				_x = value;
+			}
+		}
+		public int Y
+		{
+			get
+			{
+				return _y;
+			}
+			set
+			{
+				_y = value;
+			}
+		}
+		public Point(int x = 0, int y = 0)
+		{
+			X = x;
+			Y = y;
+		}
+		public Point(Point point)
+		{
+			X = point.X;
+			Y = point.Y;
+		}
+	}
+}
